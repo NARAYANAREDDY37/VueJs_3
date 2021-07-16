e9bcd4020d39dffb3afebe9ce0c24989a329b2b5
NARAYANAREDDY37 <narayanareddychinnapureddy@yahoo.com>
2021-07-16 19:59:08 +0530

    event bindin completed

---
+++ Vue3_EventBinding/app.js
@@ -0,0 +1,22 @@
+// creating vue app
+const app = Vue.createApp({
+	data() {
+		return {
+			count: 0,
+			name: ''
+		};
+	},
+	methods: {
+		onIncrement() {
+			this.count++;
+		},
+		onDecrement() {
+			this.count--;
+		},
+		setName(event, lastName) {
+			this.name = event.target.value + lastName;
+		}
+	}
+});
+
+app.mount('#app');
