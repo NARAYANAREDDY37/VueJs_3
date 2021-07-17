3415895ea76084e0beda483b306e7fe3cdd61054
NARAYANAREDDY37 <narayanareddychinnapureddy@yahoo.com>
2021-07-17 11:34:22 +0530

    event binding completed

--- Vue3_EventBinding/app.js
+++ Vue3_EventBinding/app.js
@@ -3,22 +3,43 @@ const app = Vue.createApp({
 	data() {
 		return {
 			count: 0,
-			name: 'Chinnapu Reddy'
+			name: 'Chinnapu Reddy',
+			confirmedName: ''
 		};
 	},
+	watch: {
+		count(value){
+			if(value > 5){
+				this.count = 0;
+			}
+		}
+	},
+	computed: {
+		fullName() {
+			console.log('executed computed');
+			return this.name;
+		}
+	},
 	methods: {
+		outputFullName() {
+			console.log('outputfull name is executed');
+			return this.name;
+		},
+		setConfirmedName() {
+			this.confirmedName = this.name;
+		},
+		onSubmit(event) {
+			// event.preventDefault();
+			alert('form submitted');
+		},
+		/* setName(event) {
+			this.name = event.target.value;
+		}, */
 		onIncrement() {
 			this.count++;
 		},
 		onDecrement() {
 			this.count--;
-		},
-		setName(event, lastName) {
-			this.name = event.target.value + lastName;
-		},
-		onSubmit(event) {
-			// event.preventDefault();
-			alert('form submitted');
 		}
 	}
 });
